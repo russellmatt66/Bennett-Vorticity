5a0a6bcf152d48d48fc44156d327b9d8eb7c3723
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-02-16 14:46:43 -0500

    Drifts.py
    Module for drift calculations

---
+++ modules/drifts.py
@@ -0,0 +1,36 @@
+"""
+Calculates the equilibrium drifts
+o Diamagnetic
+o Grad-B
+o ExB (non-uniform)
+o Gravitational (Shell)
+o Gravitational (Hot-dog: g_{0} = g_0\hat{z}_{0} = g_{0}\hat{x})
+o Resistive
+"""
+
+import numpy as np
+import constants as cnst
+
+def diamagnetic_drift() -> np.ndarray:
+    # IMPLEMENT
+    pass
+
+def gradB_drift() -> np.ndarray:
+    # IMPLEMENT
+    pass
+
+def ExB_drift() -> np.ndarray:
+    # IMPLEMENT
+    pass
+
+def gravitational_drift_shell() -> np.ndarray:
+    # IMPLEMENT
+    pass
+
+def gravitational_drift_hotdog() -> np.ndarray:
+    # IMPLEMENT
+    pass
+
+def resistive_drift() -> np.ndarray:
+    # IMPLEMENT
+    pass
