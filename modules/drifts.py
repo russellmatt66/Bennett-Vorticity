7e154485bcefa5ecd9f04b26d9239941b4d08d4a
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-02-17 14:26:16 -0500

    drifts.py and pp.py
    Finished adding drifts and thermal velocity to plasma properties

--- modules/drifts.py
+++ modules/drifts.py
@@ -7,33 +7,44 @@ o Gravitational (Shell)
 o Gravitational (Hot-dog: g_{0} = g_0\hat{z}_{0} = g_{0}\hat{x})
 o Resistive
 """
-
 import numpy as np
+
 import constants as cnst
+import plasma_properties as pp
 
-def diamagnetic_drift() -> np.ndarray:
-    # IMPLEMENT
-    pass
+def diamagnetic_drift(uz: np.ndarray) -> np.ndarray:
+    print('Calculating diamagnetic drift - it\'s just the velocity profile')
+    return uz
 
-def gradB_drift() -> np.ndarray:
-    # IMPLEMENT
-    pass
+def gradB_drift(mj: float, qj: float, btheta: np.ndarray, gradB: np.ndarray, uz: np.ndarray) -> np.ndarray:
+    KE_perp = 0.5 * mj * (uz**2)  # Placeholder for actual perpendicular kinetic energy calculation
+    numerator = KE_perp * gradB
+    denominator = qj * btheta**2
+    return numerator / denominator
 
-def ExB_drift() -> np.ndarray:
-    # IMPLEMENT
-    pass
+def ExB_drift(mj: float, T: float, B: np.ndarray, Z: float, uz: np.ndarray, lapU: np.ndarray) -> np.ndarray:
+    rhoL = pp.vth(T, mj) / pp.omega_ci(B, Z, mj) # Larmor radius
+    return -(uz + rhoL**2 / 4 * lapU)
 
-def gravitational_drift_shell() -> np.ndarray:
-    # IMPLEMENT
-    pass
+# Gravitational drift for the impact of self-gravity from the mass shell
+def gravitational_drift_shell(mj: float, qj: float, n0: float, L: float, btheta: np.ndarray) -> np.ndarray:
+    numerator = cnst.G * np.pi * mj * n0 * L 
+    denominator = btheta * qj
+    return numerator / denominator
 
-def gravitational_drift_hotdog() -> np.ndarray:
-    # IMPLEMENT
-    pass
+# Gravitational drift for hot-dog geometry, where g0 is in the x-direction instead of z-direction
+def gravitational_drift_hotdog(mj: float, qj: float, g0: float, theta: float, btheta: np.ndarray) -> np.ndarray:
+    numerator = mj * g0 * np.cos(theta)
+    denominator = btheta * qj
+    return numerator / denominator
+
+# Gravitational drift for waterfall geometry, where g0 is in the axial direction
+def gravitational_drift_waterfall(mj: float, qj: float, g0: float, btheta: np.ndarray) -> np.ndarray:
+    numerator = mj * g0
+    denominator = btheta * qj
+    return -numerator / denominator # points in negative r direction
 
 def resistive_drift(qj: float, n0: float, uz: np.ndarray, btheta: np.ndarray) -> np.ndarray:
-    # IMPLEMENT
-    # pass
     numerator = qj * cnst.q_e * n0 * uz
     denominator = btheta
     return numerator / denominator
--- modules/plasma_properties.py
+++ modules/plasma_properties.py
@@ -10,14 +10,15 @@ def omega_ce(B: float) -> float:
     '''
     return cnst.q_e * B / cnst.me 
 
-def omega_ci(B: float, Z: float) -> float:
+def omega_ci(B: float, Z: float, mj: float) -> float:
     '''
     Ion (Hydrogen) cyclotron frequency
     Units: [rad/s]
     o B - Magnetic field (T)
     o Z - Ionization state
+    o mj - Mass of the ion (kg)
     '''
-    return Z * cnst.q_e * B / cnst.mH
+    return Z * cnst.q_e * B / mj
 
 def vA(B: float, n: float) -> float:
     '''
@@ -27,3 +28,12 @@ def vA(B: float, n: float) -> float:
     o n - Plasma density (m^-3)
     '''
     return B / np.sqrt(cnst.mu0 * cnst.mH * n)
+
+def vth(T: float, mj: float) -> float:
+    '''
+    Thermal velocity
+    Units: [m/s]
+    o T - Temperature (K)
+    o mj - Mass of the particle (kg)
+    '''
+    return np.sqrt(2 * cnst.kB * T / mj)
