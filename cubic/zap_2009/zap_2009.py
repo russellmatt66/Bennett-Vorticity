8537d41446cf46df30754f456575b5b126b62366
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-02-26 22:32:48 -0500

    Plotted Zap 2009 data
    Next step is making vortex fits

--- cubic/zap_2009/zap_2009.py
+++ cubic/zap_2009/zap_2009.py
@@ -22,39 +22,64 @@ uz_data = pd.read_csv('../../experimental_data/zap_2009/zap2009_uz_fig9.csv', he
 # uz_tau_neg_0pt10 = uz_data['uz_tau_neg_0pt10'].dropna()
 uz_tau_neg_0pt10 = pd.DataFrame({
     'r (mm)': uz_data['uz_tau_neg_0pt10'],
-    'uz (km/s)': uz_data['Unnamed: 1']
+    'uz (km/s)': uz_data['Unnamed: 1'],
+    'name' : '-0.10'
 }).dropna()
 
 uz_tau_0pt10 = pd.DataFrame({
     'r (mm)': uz_data['uz_tau_0pt10'],
-    'uz (km/s)': uz_data['Unnamed: 3']
+    'uz (km/s)': uz_data['Unnamed: 3'],
+    'name' : '0.10'
 }).dropna()
 
 uz_tau_0pt16 = pd.DataFrame({
     'r (mm)': uz_data['uz_tau_0pt16'],
-    'uz (km/s)': uz_data['Unnamed: 5']
+    'uz (km/s)': uz_data['Unnamed: 5'],
+    'name' : '0.16'
 }).dropna()
 
 uz_tau_0pt34 = pd.DataFrame({
     'r (mm)': uz_data['uz_tau_0pt34'],
-    'uz (km/s)': uz_data['Unnamed: 7']
+    'uz (km/s)': uz_data['Unnamed: 7'],
+    'name': '0.34'
 }).dropna()
 
 uz_tau_0pt56 = pd.DataFrame({
     'r (mm)': uz_data['uz_tau_0pt56'],
-    'uz (km/s)': uz_data['Unnamed: 9']
+    'uz (km/s)': uz_data['Unnamed: 9'],
+    'name': '0.56'
 }).dropna()
 
 print(f'uz_tau_neg_0pt10: {uz_tau_neg_0pt10}')
-# print(f'uz_tau_0pt10: {uz_tau_0pt10}')
-# print(f'uz_tau_0pt16: {uz_tau_0pt16}')
-# print(f'uz_tau_0pt34: {uz_tau_0pt34}')
-# print(f'uz_tau_0pt56: {uz_tau_0pt56}')
+print(f'uz_tau_0pt10: {uz_tau_0pt10}')
+print(f'uz_tau_0pt16: {uz_tau_0pt16}')
+print(f'uz_tau_0pt34: {uz_tau_0pt34}')
+print(f'uz_tau_0pt56: {uz_tau_0pt56}')
 
 # uz_data.columns = ['Radius (mm)', 'uz (10^{4} m / s)']
 
 # r_data = uz_data['Radius (mm)'].to_numpy() * 1e-3 # Convert to meters
 # uz_data = uz_data['uz (10^{4} m / s)'].to_numpy() * 1e4 # Convert to m/s
+
+uz_df_list = [uz_tau_neg_0pt10, uz_tau_0pt10, uz_tau_0pt16, uz_tau_0pt34, uz_tau_0pt56]
+uzpos_list = []
+uzneg_list = []
+rpos_list = []
+rneg_list = []
+for uz_df in uz_df_list:
+    r_data = uz_df['r (mm)'].to_numpy() * 1e-3 # Convert to meters
+    uz_data = uz_df['uz (km/s)'].to_numpy() * 1e3 # Convert to m/s
+
+    uzpos = uz_data[r_data > 0]
+    uzneg = uz_data[r_data < 0] 
+    rpos = r_data[r_data > 0]
+    rneg = r_data[r_data < 0] 
+
+    uzpos_list.append(uzpos)
+    uzneg_list.append(uzneg)
+    rpos_list.append(rpos)
+    rneg_list.append(rneg)
+
 
 # uzpos = uz_data[r_data > 0]
 # uzneg = uz_data[r_data < 0] 
@@ -73,3 +98,20 @@ Make fits of Bennett vortices to each half-chord
 cbt = []
 uzpos_fits = []
 uzneg_fits = []
+
+"""
+Plot
+"""
+for i, uz_df in enumerate(uz_df_list):
+    # cbt_temp = cpfm.cbt(n0, uz0_mag, rp, Tp) # Vortex constant [m]
+    # print(f'cbt for uz0 = {uz0_mag} m/s: {cbt_temp} m')
+    # cbt.append(cbt_temp)
+    plt.figure()
+    plt.plot(uz_df['r (mm)'], uz_df['uz (km/s)'])
+    plt.title(f'Axial Velocity, Zap 2009, $\\tau$ = {uz_df["name"].iloc[0]}')
+    # plt.plot(rpos_list[i], uzpos_list[i])
+    # plt.plot(rneg_list[i], uzneg_list[i])
+    # uzpos_fit = cpfm.uz_chi2cubic_posbulk(cbt_temp, uz0_mag, u0, rpos_list[i])
+    # uzneg_fit = cpfm.uz_chi2cubic_negbulk(cbt_temp, uz0_mag, u0, -rneg_list[i]) # Make rneg positive for calculating
+    
+plt.show()
