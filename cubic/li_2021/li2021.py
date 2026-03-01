6f50815a2350fd3feeb708aa78a616e938170453
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-03-01 17:49:23 -0500

    Li et al Air Plasma Streamer
    Split Iz into front and wake. Calculated front roots. Determined proportionality constant between intensity and plasma current density.

--- cubic/li_2021/li2021.py
+++ cubic/li_2021/li2021.py
@@ -15,57 +15,79 @@ import pandas as pd
 
 from sklearn.metrics import mean_squared_error
 
-uz_data = pd.read_csv('../../experimental_data/li_2021/li2021_airplasmastreamer_fig3.csv', header=0, skiprows=[1])
+I_data = pd.read_csv('../../experimental_data/li_2021/li2021_airplasmastreamer_fig3.csv', header=0, skiprows=[1])
 
 # print(uz_data.head())
 
-uz_Iz = pd.DataFrame({
-    'r (mm)': uz_data['Figure3_Iz'],
-    'Iz (A.U.)' : uz_data['Unnamed: 1'], # not currents, but intensities of light emission
+Iz = pd.DataFrame({
+    'r (mm)': I_data['Figure3_Iz'],
+    'Iz (A.U.)' : I_data['Unnamed: 1'], # not currents, but intensities of light emission
     # 'name' : 'Iz'
 }).dropna().sort_values(by='r (mm)', ascending=True).drop_duplicates(subset=['r (mm)'])
 
-uz_Ix = pd.DataFrame({
-    'r (mm)': uz_data['Figure3_Ix'],
-    'Ix (A.U.)' : uz_data['Unnamed: 3'],
+Ix = pd.DataFrame({
+    'r (mm)': I_data['Figure3_Ix'],
+    'Ix (A.U.)' : I_data['Unnamed: 3'],
     # 'name' : 'Ix'
 }).dropna().sort_values(by='r (mm)', ascending=True) 
 
-print(uz_Iz.head())
-print(uz_Ix.head())
+print(Iz.head())
+print(Ix.head())
 
-uz_needletip_data = pd.read_csv('../../experimental_data/li_2021/Figure3_Iz_Needletip.csv', header=0, skiprows=[1])
+Iz_needletip_data = pd.read_csv('../../experimental_data/li_2021/Figure3_Iz_Needletip.csv', header=0, skiprows=[1])
 
-# print(uz_needletip_data.head())
+# print(Iz_needletip_data.head())
 
-uz_needletip = pd.DataFrame({
-    'r (mm)': uz_needletip_data.iloc[:, 0], # Assuming the first column is the spatial dimension
-    'Iz (A.U.)' : uz_needletip_data.iloc[:, 1], # Assuming the second column is the intensity
+Iz_needletip = pd.DataFrame({
+    'r (mm)': Iz_needletip_data.iloc[:, 0], # Assuming the first column is the spatial dimension
+    'Iz (A.U.)' : Iz_needletip_data.iloc[:, 1], # Assuming the second column is the intensity
     # 'name' : 'Iz'
 }).dropna().sort_values(by='r (mm)', ascending=True)
     
-print(uz_needletip.head())
+print(Iz_needletip.head())
 
 
 # Plasma properties
 u0 = 0.75e6 # Core flow velocity [m/s]; mm / ns -> m/s
 n0 = 1e18 # Plasma density [m^-3]; 1e17 - 1e19
-# Tp = 
+Tp = 1e3 * cnst.eV_to_K # Plasma temperature [K]; T = Te + Ti ~ 1 keV is just a guess
 
-plt.figure()
-plt.plot(uz_Iz['r (mm)'], uz_Iz['Iz (A.U.)'], label='Iz')
-plt.plot(uz_needletip['r (mm)'], uz_needletip['Iz (A.U.)'], label='Needle tip Iz')
+rp_front = 5e-3 # m
+rp_wake = 20e-3 # m 
 
-plt.xlabel('r (mm)')
-plt.ylabel('Intensity (A.U.)')
-plt.title('Light emission profiles from Li et al. 2021 Figure 3')
-plt.legend()
+Iz_front = Iz[Iz['r (mm)'] < 50] * 1e-3 # Convert to meters 
+Iz_wake = Iz[Iz['r (mm)'] > 50] * 1e-3 # Convert to meters
 
-plt.figure()
-plt.plot(uz_Ix['r (mm)'], uz_Ix['Ix (A.U.)'], label='Ix')
-plt.xlabel('r (mm)')
-plt.ylabel('Intensity (A.U.)')
-plt.title('Light emission profiles from Li et al. 2021 Figure 3')
-plt.legend()
+I0 = Iz['Iz (A.U.)'].max() # Use the maximum intensity as a proxy for the core flow velocity
+alpha = I0 / (cnst.q_e * n0 * u0) # Proportionality constant to convert intensity to velocity
+print(f'Proportionality constant alpha: {alpha}')
+
+uz0_roots_front = cpfm.root_solve_chi2_negbulk(Iz_front['Iz (A.U.)'] * alpha, u0, n0, rp_front, Tp)
+uz0_roots_wake = cpfm.root_solve_chi2_negbulk(Iz_wake['Iz (A.U.)'] * alpha, u0, n0, rp_wake, Tp)
+
+for uz0 in uz0_roots_front:
+    print(f'uz0 root for front: {uz0} m/s')
+    # Construct flow profile
+    # Plot against data
+
+
+# plt.figure()
+
+
+# plt.figure()
+# plt.plot(Iz['r (mm)'], Iz['Iz (A.U.)'], label='Iz')
+# plt.plot(Iz_needletip['r (mm)'], Iz_needletip['Iz (A.U.)'], label='Needle tip Iz')
+
+# plt.xlabel('r (mm)')
+# plt.ylabel('Intensity (A.U.)')
+# plt.title('Light emission profiles from Li et al. 2021 Figure 3')
+# plt.legend()
+
+# plt.figure()
+# plt.plot(Ix['r (mm)'], Ix['Ix (A.U.)'], label='Ix')
+# plt.xlabel('r (mm)')
+# plt.ylabel('Intensity (A.U.)')
+# plt.title('Light emission profiles from Li et al. 2021 Figure 3')
+# plt.legend()
 
 plt.show()
