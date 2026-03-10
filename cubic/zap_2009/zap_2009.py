7b08b067b7442f2ed915e4c133e6fbcca3827fd3
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-03-10 00:17:20 -0400

    Update zap_2009.py
    Solved equations with boundary data for analytic vortices based on a base-flow, positive, cubic, chi=2 model. Haven't tested anything in here.

--- cubic/zap_2009/zap_2009.py
+++ cubic/zap_2009/zap_2009.py
@@ -20,6 +20,7 @@ uz_data = pd.read_csv('../../experimental_data/zap_2009/zap2009_uz_fig9.csv', he
 # print(uz_data.columns)
 
 # uz_tau_neg_0pt10 = uz_data['uz_tau_neg_0pt10'].dropna()
+# ***Could get rid of the 'name' field - just put tau into an array
 uz_tau_neg_0pt10 = pd.DataFrame({
     'r (mm)': uz_data['uz_tau_neg_0pt10'],
     'uz (km/s)': uz_data['Unnamed: 1'],
@@ -62,29 +63,29 @@ print(f'uz_tau_0pt56: {uz_tau_0pt56}')
 # uz_data = uz_data['uz (10^{4} m / s)'].to_numpy() * 1e4 # Convert to m/s
 
 uz_df_list = [uz_tau_neg_0pt10, uz_tau_0pt10, uz_tau_0pt16, uz_tau_0pt34, uz_tau_0pt56]
-uzpos_list = []
-uzneg_list = []
-rpos_list = []
-rneg_list = []
+# uzpos_list = []
+# uzneg_list = []
+# rpos_list = []
+# rneg_list = []
 
 uedge_pos = []
 uedge_neg = []
 u0 = []
 
 for uz_df in uz_df_list:
-    r_data = uz_df['r (mm)'].to_numpy() * 1e-3 # Convert to meters
-    uz_data = uz_df['uz (km/s)'].to_numpy() * 1e3 # Convert to m/s
+    # Doesn't seem to be a point to the below
+    # r_data = uz_df['r (mm)'].to_numpy() * 1e-3 # Convert to meters
+    # uz_data = uz_df['uz (km/s)'].to_numpy() * 1e3 # Convert to m/s
 
-    uzpos = uz_data[r_data > 0]
-    uzneg = uz_data[r_data < 0] 
-    rpos = r_data[r_data > 0]
-    rneg = r_data[r_data < 0] 
+    # uzpos = uz_data[r_data > 0]
+    # uzneg = uz_data[r_data < 0] 
+    # rpos = r_data[r_data > 0]
+    # rneg = r_data[r_data < 0] 
 
-    uzpos_list.append(uzpos)
-    uzneg_list.append(uzneg)
-    rpos_list.append(rpos)
-    rneg_list.append(rneg)
-
+    # uzpos_list.append(uzpos)
+    # uzneg_list.append(uzneg)
+    # rpos_list.append(rpos)
+    # rneg_list.append(rneg)
     uedge_pos.append(uz_df.loc[uz_df['r (mm)'] == uz_df['r (mm)'].max(), 'uz (km/s)'].values[0] * 1e3) # Convert to m/s
     # uedge_pos.append(uzpos[np.where(r_data == r_data.max())[0][0]] * 1e3) # Convert to m/s
     uedge_neg.append(uz_df.loc[uz_df['r (mm)'] == uz_df['r (mm)'].min(), 'uz (km/s)'].values[0] * 1e3) # Convert to m/s
@@ -109,8 +110,12 @@ Tp = 200 * cnst.eV_to_K # Plasma temperature [K]; T = Te + Ti = 150 - 200 eV
 # uedge = 4e4 # Edge flow velocity [m/s]; 
 # u0 = 10e4 # Core flow velocity [m/s]; 
 
-cbt_pos = []
-cbt_neg = []
+# c.f. Figure 8. of Shumlak et. al (2009) Nucl Fusion 49 075039, which defines normalized time tau w.r.t proper time pulse
+def t(tau):
+    return 44 * tau + 34
+
+cbts_pos = []
+cbts_neg = []
 
 # Manually recorded from Figure 9 of Shumlak et. al (2009) Nucl Fusion 49 075039
 rp_pos = [15e-3, 5e-3, 15e-3, 15e-3, 10e-3] # Pinch radius [m];
@@ -119,13 +124,51 @@ rp_neg = [20e-3, 25e-3, 15e-3, 15e-3, 25e-3]
 uzpos_fits = []
 uzneg_fits = []
 
+num_r = 100
+r_uzpos = []
+r_uzneg = []
+
+for ir in range(len(rp_pos)):
+    r_uzpos.append(np.linspace(0, rp_pos[ir], num_r))
+    r_uzneg.append(np.linspace(0, rp_neg[ir], num_r))
+
+uzpos_roots = []
+uzneg_roots = []
+"""
+Solve
+"""
+for i in range(len(uz_df_list)):
+    uz0_temp_pos = cpfm.root_solve_chi2_posbulk(uedge_pos[i], u0[i], n0, rp_pos[i], Tp)
+    uz0_temp_neg = cpfm.root_solve_chi2_posbulk(uedge_neg[i], u0[i], n0, rp_neg[i], Tp)
+    uzpos_roots.append(uz0_temp_pos)
+    uzneg_roots.append(uz0_temp_neg)
+    cbts_pos_temp = []
+    cbts_neg_temp = []
+    uzpos_temp = []
+    uzneg_temp = []
+    for uz0 in uz0_temp_pos:
+        cbt_pos = cpfm.cbt(n0, np.abs(uz0), rp_pos[i], Tp) # Vortex constant [m]
+        cbt_neg = cpfm.cbt(n0, np.abs(uz0), rp_neg[i], Tp) # Vortex constant [m]
+        uzpos_fit = cpfm.uz_chi2cubic_posbulk(cbt_pos, np.abs(uz0), u0[i], r_uzpos[i])
+        uzneg_fit = cpfm.uz_chi2cubic_posbulk(cbt_neg, np.abs(uz0), u0[i], r_uzneg[i])
+        cbts_pos_temp.append(cbt_pos)
+        cbts_neg_temp.append(cbt_neg)
+        uzpos_temp.append(uzpos_fit)
+        uzneg_temp.append(uzneg_fit)
+
+    cbts_pos.append(tuple(cbts_pos_temp))
+    cbts_neg.append(tuple(cbts_neg_temp))
+    uzpos_fits.append(uzpos_temp)
+    uzneg_fits.append(uzneg_temp)
+
+    # cbt_temp = cpfm.cbt(n0, uz0_mag, rp, Tp) # Vortex constant [m]
+    # print(f'cbt for uz0 = {uz0_mag} m/s: {cbt_temp} m')
+    # cbt.append(cbt_temp)
+
 """
 Plot
 """
 for i, uz_df in enumerate(uz_df_list):
-    # cbt_temp = cpfm.cbt(n0, uz0_mag, rp, Tp) # Vortex constant [m]
-    # print(f'cbt for uz0 = {uz0_mag} m/s: {cbt_temp} m')
-    # cbt.append(cbt_temp)
     plt.figure()
     plt.plot(uz_df['r (mm)'], uz_df['uz (km/s)'])
     plt.title(f'Axial Velocity, Zap 2009, $\\tau$ = {uz_df["name"].iloc[0]}')
