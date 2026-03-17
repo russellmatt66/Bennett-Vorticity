c3a5e60ea4912392774599009258665a819310ce
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-03-17 15:51:44 -0400

    Update vortex.py
    Adding the necessary lists

--- modules/vortex.py
+++ modules/vortex.py
@@ -2,12 +2,19 @@
 Class for fitting a single vortex to experimental data
 """
 class Vortex:
-    def __init__(self, n0, Tp, uedge, u0, rp):
+    def __init__(self, n0, Tp, uedge, u0, rp, num_r=100):
         self.n0 = n0
         self.Tp = Tp
         self.uedge = uedge
         self.u0 = u0
         self.rp = rp
+        self.num_r = num_r
+        # Initialize the necessary lists
+        self.cbts = []
+        self.uzpos_fits = []
+        self.uzneg_fits = []
+        self.uzpos_roots = []
+        self.uzneg_roots = []
     
     # Load data to fit 
     def load_data(self, filepath):
