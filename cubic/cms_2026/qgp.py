77fb041f23b8c6ff893a6ab619cbab99545f50a1
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-04-07 17:31:49 -0400

    Update qgp.py
    Closing QGP investigation for the moment. Needs significant analysis to connect observable, e.g., the azimuthal coefficient to underlying QGP Bennett vortex

--- cubic/cms_2026/qgp.py
+++ cubic/cms_2026/qgp.py
@@ -1,4 +1,7 @@
 """
 Can Bennett vortices be meaningfully fit to this: https://ars.els-cdn.com/content/image/1-s2.0-S0370269325008767-gr1_lrg.jpg
 From: https://www.sciencedirect.com/science/article/pii/S0370269325008767?via%3Dihub#fig0001
+
+No, a better QGP observable to compare would be, for example, the azimuthal flow coefficient but this requires significant 
+analysis first that is outside the scope of this work  
 """
