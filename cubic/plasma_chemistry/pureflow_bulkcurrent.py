63116dd397e75a4bd0a81c21afcddb055e2bb8ad
Matt <47687321+russellmatt66@users.noreply.github.com>
2026-04-14 21:19:37 -0400

    Update pureflow_bulkcurrent.py
    Calculate waterfall drifts without large numerical oscillations from logarithmic terms

--- cubic/plasma_chemistry/pureflow_bulkcurrent.py
+++ cubic/plasma_chemistry/pureflow_bulkcurrent.py
@@ -1,3 +1,6 @@
 """
 Investigating the behavior of a cubic pure-flow velocity and a bulk cubic plasma current for pollutant ion deposition
+
+o Compute flow profile -> Compute magnetic field -> Compute waterfall drift speed
+
 """
