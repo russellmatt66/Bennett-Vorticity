Fixed a units bug in the Coulomb log calculation

modules/cubic_pureflow_module.py
@@ -165,12 +165,12 @@ def btheta_chi2(cbt: float, uz0: float, n0: float, r: np.ndarray) -> np.ndarray:
     '''
     Magnetic field profile btheta(r) for cubic pureflow vortex
     Units: [T]
-    B_{theta}^{(2)}(r) = -mu0 * e * uz0 * n0 * f(cbt, r) / (2 * r * (r + cbt))
+    B_{theta}^{(2)}(r) = mu0 * e * uz0 * n0 * f(cbt, r) / (2 * r * (r + cbt))
                     = \frac{mu0}{r} * \int_0^r' r' * J_z^{(2)}(r') dr' 
     '''
-    term1 = cnst.mu0 * cnst.q_e * uz0 * n0
+    term1 = cnst.mu0 * cnst.q_e * np.abs(uz0) * n0
     term2 = f(cbt, r) / (2.0 * r * (r + cbt))
-    return -term1 * term2
+    return term1 * term2
 
 def btheta_chi2_negbulk(cbt: float, uz0: float, u0: float, n0: float, r: np.ndarray) -> np.ndarray:
     '''

modules/spitzer.py
@@ -85,9 +85,10 @@ def coulombLog_ee(n0: float, Tp: float) -> float:
     Coulomb logarithm
     Units: dimensionless
     o n0 - Edge plasma density (m^-3)
-    o Tp - Edge plasma temperature (K)
+    o Tp - Edge plasma temperature (eV)
     '''
-    if Tp * cnst.K_to_eV < 10:
+    Tp = Tp * cnst.K_to_eV 
+    if Tp < 10:
         return 23 - np.log(n0**0.5 * Tp**-1.5)
     else:
         return 24 - np.log(n0**0.5 * Tp**-1)
@@ -97,10 +98,11 @@ def coulombLog_ei(n0: float, Tp: float, Z: float) -> float:
     Coulomb logarithm
     Units: dimensionless
     o n0 - Edge plasma density (m^-3)
-    o Tp - Edge plasma temperature (K)
+    o Tp - Edge plasma temperature (eV)
     o Z - Ionization state
     '''
-    if Tp * cnst.K_to_eV < 10 * Z**2:
+    Tp = Tp * cnst.K_to_eV 
+    if Tp < 10 * Z**2:
         return 23 - np.log(n0**0.5 * Tp**-1.5) + np.log(Z)
     else:
         return 24 - np.log(n0**0.5 * Tp**-1)